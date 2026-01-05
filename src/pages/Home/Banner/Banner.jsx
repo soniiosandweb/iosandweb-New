bc7a84fadce7245c01517c2cbfc4b247869df82b
hardeep-2025 <hardeep@iosandweb.net>
2026-01-05 19:04:36 +0530

    banner heading change

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,42 +1,20 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./Banner.css";
 import TypeWritter from "../../../components/TypeWritter";
-import { useState } from "react";
-
 
 const homeBanner = `${process.env.REACT_APP_API_URL}/assests/home/home-banner.webp`;
 
 // Home video
 const homeVideo = `${process.env.REACT_APP_API_URL}/assests/home-video.mp4`;
 
-
 const Banner = () => {
-      const [showSecond, setShowSecond] = useState(false);
-
     return(
         <div className="home-banner">
             <Container>
                 <Row>
                     <Col>
                         <div className="intro-content">
-                        <h1 className="heading_main bannerPadding">
-                        <TypeWritter
-                            text="We design and deliver intelligent, scalable technology solutions that help businesses innovate, automate, and grow faster."
-                            delay={100}
-                            onComplete={() => setShowSecond(true)}
-                        />
-
-                        {showSecond && (
-                            <>
-                            <br /><br/>
-                            <TypeWritter
-                                text="Intelligent solutions built to optimize performance, enhance efficiency, and accelerate business growth."
-                                delay={100}
-                                infinite
-                            />
-                            </>
-                        )}
-                        </h1>
+                            <h1 className="heading_main"><TypeWritter text="Empowering businesses with intelligent technology solutions for a smarter future." delay={100} infinite  /></h1>
                         </div>
                         <video className="intro_video" poster={homeBanner} autoPlay={true} muted={true} loop={true}>
                             <source src={homeVideo} type="video/mp4"></source>
